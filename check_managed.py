28ed14afd8c94f6ccdefdfd3b7ef76565096761c
UniformOkCu <trunghuunguyen1028@gmail.com>
2024-10-18 18:04:30 -0400

    Updated check_managed.py

--- check_managed.py
+++ check_managed.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import xml.etree.ElementTree as ET
+import json
 
 def check_managed(domain):
     res = requests.get(f"https://login.microsoftonline.com/getuserrealm.srf?login={domain}&xml=1")
@@ -18,6 +19,21 @@ def check_managed(domain):
     
     return None
 
+def get_tenant_id(domain):
+    res = requests.get(f"https://login.microsoftonline.com/{domain}/.well-known/openid-configuration")
+    data = res.content
+
+    json_str = data.decode('utf-8')
+
+    json_data = json.loads(json_str)
+
+    token_endpoint = json_data['token_endpoint']
+
+    tenant_id = str(token_endpoint).split(".com/")[1].split("/")[0]
+
+    return tenant_id
+    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--domain', help="Domain to check", required=True)
@@ -26,6 +42,6 @@ if __name__ == "__main__":
     domain = args.domain
 
     if (check_managed(domain)):
-        print(f"{domain} is managed.")
+        print(get_tenant_id(domain))
     else:
-        print(f"{domain} is NOT managed.")
+        print(f"{domain} not managed")
